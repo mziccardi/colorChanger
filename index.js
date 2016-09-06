4470085b618b42b1187ca9870d73b2ad3852d1fa
Mike Ziccardi <mikeziccardi@gmail.com>
2016-09-06 15:07:29 -0600

    now responsive at 500px

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ var colorButton = $('.randomButton');
 var resetButton = $('.reset');
 
 colorButton.on('click',function(){
-  $('body').css('background',"rgb" + randomColor())
+  $('body').css('background',"rgb" + randomColor());
 });
 
-  resetButton.on('click',function(){
+resetButton.on('click',function(){
     $('body').css('background','white');
-  });
+});
+
+
 
 function randomColor(){
-
 return '('+ (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
 }
-
-// function resetColor() {
-//
-// }
